577717f041269acdbf522b145901d5144321a6a8
Axel Dolce <axel.dolce@gmail.com>
2017-02-28 13:25:59 -0300

    refactor

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,61 +1,71 @@
 import React from 'react'
-import { Link, Route } from 'react-router-dom'
-import { List } from './UI'
+import { NavLink, Route } from 'react-router-dom'
+
+import { Card } from './UI'
 import ProductsRouter from '../routes/Products'
+import VendorsRouter from '../routes/Vendors'
+
+import Navbar from '../containers/Navbar'
+import LoginForm from '../components/LoginForm'
 
 
-var links = [{
-    name: 'Home',
-    path: '/'
-}, {
-    name: 'Products',
-    path: '/app/products'
-}, {
-    name: 'Vendors',
-    path: '/app/vendors'
-}]
 
 function Sidebar(props) {
-    return <div style={{
-        backgroundColor: 'gainsboro',
+    return <div className="bg-15" style={{
         padding: '1rem',
-        width: '10%'
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)'
     }} {...props}></div>
 }
 function Content(props) {
-    return <div style={{
-        padding: '1rem',
-        width: '90%'
-    }} {...props}></div>
+    return <div {...props} style={Object.assign({
+        padding: '0.5rem 1rem 1rem',
+        width: '100%'
+    }, props.style)}></div>
 }
 
 class MainLayout extends React.Component {
-    componentWillMount(){
+    componentWillMount() {
         window.addEventListener('resize', this._update.bind(this))
     }
-    componentWillUmount(){
+    componentWillUmount() {
         window.removeEventListener('resize', this._update.bind(this))
     }
-    _update(){
+    _update() {
         this.setState({})
     }
     render() {
-        var { match } = this.props
         return (
-            <div className="MainLayout" style={{ height: window.innerHeight, overflow: 'hidden' }}>
-                <div>NavBar goes here</div>
-                <div className="flex" style={{ height: '100%' }}>
+            <div className="MainLayout max-height">
+                <div style={{ height: '9%' }}>
+                    <Navbar />
+                </div>
+                <div className="flex" style={{ height: '91%', backgroundColor: 'gainsboro' }}>
                     <Sidebar>
-                        <List>
-                            {links.map(({ name, path, absolute }) => (
-                                <div key={path}>
-                                    <Link to={path}>{name}</Link>
-                                </div>
-                            ))}
-                        </List>
+                        <ul className="nav nav-pills nav-stacked">
+                            <li>
+                                <NavLink to="/products" activeStyle={{ color: 'white', backgroundColor: 'hsla(190, 100%, 40%, 1)' }}>
+                                    P
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/vendors" activeStyle={{ color: 'white', backgroundColor: 'hsla(190, 100%, 40%, 1)' }}>
+                                    V
+                                </NavLink>
+                            </li>
+                        </ul>
                     </Sidebar>
-                    <Content>
-                        <Route path={match.url + '/products'} component={ProductsRouter} />
+                    <Content style={{ overflow: 'hidden' }}>
+                        <div className="flex" style={{ height: '100%' }}>
+                            <Card style={{ backgroundColor: 'hsla(270, 50%, 80%, 1)', width:'100%' }}>
+                                <Route path={'/products'} component={ProductsRouter} />
+                                <Route path={'/vendors'} component={VendorsRouter} />
+                                <Route path={'/login'} render={(props) => (
+                                    <Card>
+                                        <LoginForm {...props} />
+                                    </Card>
+                                )} />
+                            </Card>
+                        </div>
                     </Content>
 
                 </div>
