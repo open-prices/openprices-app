42395fcaaa1017a9c4915de4d56c3e8cd0d64ceb
Axel Dolce <axel.dolce@gmail.com>
2017-02-28 13:27:11 -0300

    vendors router

---
+++ src/routes/Vendors.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+import { Card } from '../components/UI'
+
+import path from 'path'
+
+class Vendors extends React.Component {
+
+    render() {
+        var { match } = this.props
+        return (
+            <div>
+                <Route exact path={match.url} component={(props) => (
+                    <Card>
+                        <VendorsList {...props} />
+                    </Card>
+                )} />
+                <Route path={path.join(match.path, '/:id')} render={(props) => (
+                    null
+                )} />
+            </div>
+        )
+    }
+}
+
+export default Vendors
+
+var VendorsList = connect()(function VendorsList(props) {
+    return null
+})
