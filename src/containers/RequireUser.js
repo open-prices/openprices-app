9d5ef5cd3c27c05ed9f1c5408b034c061a6b72c2
Axel Dolce <axel.dolce@gmail.com>
2017-02-28 23:11:12 -0300

    require user middleware

---
+++ src/containers/RequireUser.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+
+class RequireUser extends React.Component {
+    render() {
+        var { username } = this.props
+        
+        if (!username) return (
+            <Redirect to={{
+                pathname: '/login',
+                state: { from: this.props.location }
+            }} />
+        )
+
+        return this.props.children
+    }
+}
+
+export default connect((state, ownProps) => {
+    return {
+        username: state.user.username
+    }
+})(RequireUser)
