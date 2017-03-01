4a643ba30287299fb9f66bd43628600d8f7ffeb5
Axel Dolce <axel.dolce@gmail.com>
2017-02-28 23:08:45 -0300

    product create route

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
+
+import RequireUser from '../containers/RequireUser'
 
 import { Card } from '../components/UI'
+import AddProductForm from '../components/AddProductForm'
 
 import ProductsListFilters from '../containers/ProductsListFilters'
 import ProductsList from '../containers/ProductsList'
@@ -19,15 +22,26 @@ class Products extends React.Component {
                     <div>
                         <Card style={{ marginBottom: '1rem' }}>
                             <ProductsListFilters {...props} />
+                            <Link to={path.join(match.url, 'create')}>Add {match.url}</Link>
                         </Card>
                         <Card>
                             <ProductsList {...props} />
                         </Card>
                     </div>
                 )} />
-                <Route path={path.join(match.path, '/:barcode')} render={(props) => (
-                    <ProductDetails {...props} barcode={props.match.params.barcode} />
-                )} />
+                <Switch>
+
+                    <Route path={path.join(match.path, '/create')} render={props => (
+                        <RequireUser>
+                            <Card>
+                                <AddProductForm />
+                            </Card>
+                        </RequireUser>
+                    )} />
+                    <Route path={path.join(match.path, '/:barcode')} render={(props) => (
+                        <ProductDetails {...props} barcode={props.match.params.barcode} />
+                    )} />
+                </Switch>
             </div>
         )
     }
