d3f854f05d1d84cea255e38d6cd2196dfe19217b
Axel Dolce <axel.dolce@gmail.com>
2017-03-06 23:06:13 -0300

    save token in storage

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,11 +6,17 @@ export function login(username, password) {
     var ep = endpoint('/api/auth/token')
     var data = { username, password }
     return axios.post(ep, data).then(response => {
+        sessionStorage.access_token = response.data.jwt
+        sessionStorage.access_token_decoded = JSON.stringify(response.data.data)
         return response.data.data
     })
 }
 export function logout() {
     var ep = endpoint('/api/auth/logout')
+    
+    delete sessionStorage.access_token
+    delete sessionStorage.access_token_decoded
+
     return axios.get(ep)
 }
 export function register(username, password, nickname) {
