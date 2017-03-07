4b4739a7ec911202818cd8c7a9f6b6564b56b7b6
Axel Dolce <axel.dolce@gmail.com>
2017-03-06 23:07:14 -0300

    delete price from store

--- src/modules/products.js
+++ src/modules/products.js
@@ -1,4 +1,5 @@
 export const ADD = 'PRODUCTS/ADD'
+export const DELETE_PRICE = 'PRODUCTS/PRICES/DELETE'
 
 var init = {
     products: [],
@@ -9,6 +10,8 @@ export default function reducer(state = init, action) {
     switch (action.type) {
         case ADD:
             return onAdd(state, action)
+        case DELETE_PRICE:
+            return onDeletePrice(state, action)
         default:
             return state
     }
@@ -17,6 +20,12 @@ export default function reducer(state = init, action) {
 
 export function add(product) {
     return { type: ADD, payload: product }
+}
+export function deletePrice(price) {
+    return {
+        type: DELETE_PRICE,
+        payload: price
+    }
 }
 function onAdd(state, action) {
 
@@ -32,6 +41,26 @@ function onAdd(state, action) {
 
     var product = productsByBarcode[barcode]
     productsByBarcode[barcode] = Object.assign({}, product, payload)
+
+    return Object.assign({}, state, {
+        products, productsByBarcode
+    })
+
+}
+function onDeletePrice(state, action) {
+
+    var { payload } = action
+
+    var { products, productsByBarcode } = state
+
+    products.map(barcode => {
+        var product = productsByBarcode[barcode]
+        if (!product.prices) return
+        product.prices = product.prices.filter(price => {
+            return price.id !== payload.id
+        })
+        productsByBarcode[barcode] = product
+    })
 
     return Object.assign({}, state, {
         products, productsByBarcode
