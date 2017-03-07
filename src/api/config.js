af3574e8b38552ed67d51138af12030c651a7b5a
Axel Dolce <axel.dolce@gmail.com>
2017-03-06 23:31:50 -0300

    base url override

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 (axios => {
     axios.defaults.baseURL = 'http://localhost:3001'
-    axios.interceptors.request.use(function(config){
+    axios.interceptors.request.use(function (config) {
         if (sessionStorage.access_token) {
             config.headers['Authorization'] = 'Bearer ' + sessionStorage.access_token
         }
@@ -12,9 +12,11 @@ import axios from 'axios'
 
 
 export var BASE_URL = (function resolveBaseURL() {
-    
+
+    if (localStorage.BASE_URL) return localStorage.BASE_URL
+
     var { hostname, port } = window.location
-    
+
     switch (hostname) {
         case 'open-prices.github.io':
             return 'https://openprices-api.zapto.org'
@@ -26,6 +28,5 @@ export var BASE_URL = (function resolveBaseURL() {
 })()
 
 export function endpoint(path) {
-    return path
     return BASE_URL + path
 }
