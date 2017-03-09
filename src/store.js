bbc2214c839bbbd2ae5e00750a42b258a10b763a
Axel Dolce <axel.dolce@gmail.com>
2017-03-08 23:56:13 -0300

    more reducers

--- src/store.js
+++ src/store.js
@@ -2,15 +2,24 @@ import { createStore, compose, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import products from './modules/products'
+import prices from './modules/prices'
 import productsListFilters from './modules/productsListFilters'
 import vendors from './modules/vendors'
 import user from './modules/user'
+import users from './modules/users'
 
 var middleware = applyMiddleware(thunk)
 var composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ name: 'Products' }) : compose;
 var enhancers = composeEnhancers(middleware);
 
-var reducers = combineReducers({ products, productsListFilters, vendors, user })
+var reducers = combineReducers({
+    products,
+    prices,
+    productsListFilters,
+    vendors,
+    user,
+    users
+})
 
 var store = createStore(reducers, {}, enhancers)
 
